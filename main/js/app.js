a436c54cb02ec05029faee72e18c9578d6a4556b
rezamansourireza <rema49202@gmail.com>
2024-03-26 16:24:04 +0330

    part #1 navbar

---
+++ main/js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, {
+        edge: "right",
+    });
+
+
+});
